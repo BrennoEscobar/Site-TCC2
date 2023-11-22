fcf27f1c8a52a9bd69a12ea1c143c4bc24870cf3
BrennoEscobar <106408359+BrennoEscobar@users.noreply.github.com>
2023-11-22 17:30:16 -0300

    Add files via upload

---
+++ Purple.Buzz/templatemo_561_purple_buzz/dark.js
@@ -0,0 +1,14 @@
+function DarkMode() {
+    let minhaImagem = document.getElementById('imgDark');
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+
+    // Salvar a preferência do usuário em um cookie
+    if (element.classList.contains("dark-mode")) {
+        setCookie('darkMode', 'enabled', 30);
+        minhaImagem.setAttribute('src', '../img/light.png');
+    } else {
+        setCookie('darkMode', 'disabled', 30);
+        minhaImagem.setAttribute('src', '../img/dark.png');
+    }
+}
